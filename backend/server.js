e267334437183e28e90de6811985d5fa5922ad69
Rajat Mittal <rajatmittal359@gmail.com>
2025-12-08 03:39:38 +0530

    again

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@
 
     // Middleware
     app.use(express.json());
-    app.use(
-      cors({
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
-        credentials: true,
-      })
-    );
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://shopmaster-pro.onrender.com"
+  ],
+  credentials: false
+}));
     // Routes imports
     const authRoutes = require('./routes/authRoutes');
     const adminRoutes = require('./routes/adminRoutes');
