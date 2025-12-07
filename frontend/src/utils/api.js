d8302706fb83e32dba5735b21801605c5683be01
Rajat Mittal <rajatmittal359@gmail.com>
2025-12-08 03:08:25 +0530

    done

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: false,
-});
 
+const api = axios.create({
+  baseURL: "https://shopmaster-api.onrender.com/api",
+});
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('smp_token');
